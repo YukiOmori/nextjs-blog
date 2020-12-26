796f8602423cee446da702764ffca312fe9028cb
Yuki Omori <yukiomori4211@gmail.com>
2020-12-27 02:05:29 +0900

    feat: Add Links and Head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,17 @@
+import Head from 'next/head'
+import Link from 'next/link';
+
 const FirstPost = () => (
+  <>
+   <Head>
+        <title>First Post</title>
+  </Head>
   <h1>First Post</h1>
+  <h2>
+    <Link href="/">
+      <a>Back to home</a>
+    </Link>
+  </h2>
+</>
 )
 export default FirstPost;
