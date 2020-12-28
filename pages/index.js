efa7d9545ba037b3887ef18c3acfde680c7ab59e
Yuki Omori <yukiomori4211@gmail.com>
2020-12-28 16:24:13 +0900

    chore: Changed text for test

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Yuki Omori.</p>
+        <p>Hi, I'm Ikatou.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
